2086fb38ab7dcea697cdc0045346c94c94ef364b
Amkam <amaduskamara@gmail.com>
2025-08-02 09:04:53 +0000

    Use props in different components

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,10 @@ const App = () => {
       <Navbar />
 
       {/* <!-- Hero --> */}
-      <Hero />
+      <Hero
+        title='Become a React Dev'
+        subtitle='Find the React job that fits your skills and needs'
+      />
 
       {/* <!-- Developers and Employers --> */}
       <DevEmp />
