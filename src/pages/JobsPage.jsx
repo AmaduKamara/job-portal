4ad322288269da8e0c5f4b0d688f91f182ab14d5
Amkam <amaduskamara@gmail.com>
2025-08-03 13:10:55 +0000

    Add filter jobs on Jobs page

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const JobsPage = () => {
   return (
-    <div>JobsPage</div>
-  )
-}
+    <>
+      {/* <!-- Filter Jobs --> */}
+      <section class='bg-blue-50 py-4'>
+        <div class='container mx-auto px-4'>
+          <div class='relative'>
+            <input
+              type='text'
+              class='w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500'
+              placeholder='Filter jobs...'
+            />
+          </div>
+        </div>
+      </section>
 
-export default JobsPage
+      {/* <!-- All Jobs --> */}
+      <section class='bg-blue-50 px-4 py-10'></section>
+    </>
+  );
+};
+
+export default JobsPage;
