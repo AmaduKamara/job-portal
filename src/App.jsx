659a31858f0b3ef0ca2a026aa84a13bbdf153cdd
Amkam <amaduskamara@gmail.com>
2025-08-03 13:06:10 +0000

    Create add-job page and set up the route

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import {
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layout/MainLayout";
 import JobsPage from "./pages/JobsPage";
+import AddJob from "./pages/AddJob";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/add-job' element={<AddJob />} />
     </Route>
   )
 );
