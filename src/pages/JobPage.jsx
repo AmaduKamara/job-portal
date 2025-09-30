c558a69a9c7884480516483c8bf16f98c5609d11
Amkam <amaduskamara@gmail.com>
2025-09-30 07:23:06 +0000

    Modify edit job a tag to a Link tag

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -95,12 +95,12 @@ const JobPage = () => {
               {/* <!-- Manage --> */}
               <div className='bg-white p-6 rounded-lg shadow-md mt-6'>
                 <h3 className='text-xl font-bold mb-6'>Manage Job</h3>
-                <a
-                  href='/add-job.html'
+                <Link
+                  to={`/jobs/edit/${job.id}`}
                   className='bg-teal-500 hover:bg-teal-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block'
                 >
                   Edit Job
-                </a>
+                </Link>
                 <button className='bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block'>
                   Delete Job
                 </button>
